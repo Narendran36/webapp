4de047c475dcd7f62835467b6b9041953f4635a4
ManeeshM5 <103023248+ManeeshM5@users.noreply.github.com>
2022-06-22 12:55:52 +0530

    Update upload-results.py

--- upload-results.py
+++ upload-results.py
@@ -2,7 +2,7 @@ import argparse
 import requests
 from datetime import datetime
 
-#usage: python3 upload-results.py --host $ --api_key $ --engagement_name $ --scanner $ --product_name $ --result_file $ 
+#usage: python3 upload-results.py --host $ --api_key $ --engagement_name $ --scanner $ --product_name $ --result_file $ --tag $
 
 parser = argparse.ArgumentParser(description='CI/CD integration for DefectDojo')
 parser.add_argument('--host', help="DefectDojo Hostname", required=True)
@@ -11,7 +11,7 @@ parser.add_argument('--engagement_name', help="Engagement ID (optional)", requir
 parser.add_argument('--scanner', help="Type of scanner", required=True)
 parser.add_argument('--product_name', help="DefectDojo Product ID", required=False)
 parser.add_argument('--result_file', help="Path to the file to be uploaded including the file name inside the jenkins ", required=True)
-#parser.add_argument('--file_name', help="name of the file to be uploaded", required=True)
+parser.add_argument('--tag', help="name of the file to be uploaded", required=False)
 
 
 
@@ -22,14 +22,14 @@ product_name = args["product_name"]
 scanner = args["scanner"]
 engagement_id = args["engagement_name"]
 path = args["result_file"]
-#name = args["file_name"]
+tag = args["tag"]
 
 url = "http://"+host+"/api/v2/import-scan/"
 
 
 
 payload={'verified': 'true',
-'tags': 'test',
+'tags': tag,
 'scan_date' : datetime.now().strftime("%Y-%m-%d"),
 'scan_type': scanner,
 'minimum_severity': 'Info',
